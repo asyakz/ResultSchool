4e5106b0c114d1485c96768fe8b5c9ea804b69c9
anastasiia_kazantseva <asyakz@gmail.com>
2023-01-13 21:19:03 +0500

    change code to classes in index.js and utils.js

--- Donate/index.js
+++ Donate/index.js
@@ -1,40 +1,46 @@
-'use strict';
-
-import { getNowDate } from './utils/utils';
+import { FormatDate } from './utils/utils';
 import './index.css';
 
 const donateContainer = document.querySelector('.donates-container__donates');
 const input = document.querySelector('.donate-form__donate-input');
 const sum = document.querySelector('#total-amount');
-
 const btn = document.querySelector('.donate-form__submit-button');
+
 btn.addEventListener('click', (event) => {
   event.preventDefault();
   if (input.value !== '') {
-    addNewDonate();
+    const donate = new Donate(input.value);
+    donate.addNewDonate();
   }
 })
 
-function getNewSum() {
-  let inputSum = sum.textContent.split('');
-  for (let i = 0; i < inputSum.length; i++) {
-    if (inputSum[i] === '$') {
-      inputSum.splice(i, 1);
-      break;
-    }
+class Donate {
+  constructor(inputValue) {
+    this.inputValue = inputValue;
   }
-  let myNewSum = inputSum.join('');
-  return myNewSum;
-}
 
-function addNewDonate() {
-  const donateItem = document.createElement('div');
-  donateItem.className = 'donate-item';
-  donateItem.innerHTML = `${getNowDate()} - <b> ${input.value}$ </b> `;
-  donateContainer.append(donateItem);
-  let newSumofDonate = Number(getNewSum()) + Number(input.value);
-  sum.textContent = String(newSumofDonate) + '$';
-  input.value = '';
+  addNewDonate() {
+    const donateItem = document.createElement('div');
+    donateItem.className = 'donate-item';
+    const formatDate = new FormatDate();
+    donateItem.innerHTML = `${formatDate.getNowDate()} - <b> ${this.inputValue}$ </b> `;
+    donateContainer.append(donateItem);
+    let newSumofDonate = Number(this.#getNewSum()) + Number(this.inputValue);
+    sum.textContent = String(newSumofDonate) + '$';
+    input.value = '';
+  }
+
+  #getNewSum() {
+    let inputSum = sum.textContent.split('');
+    for (let i = 0; i < inputSum.length; i++) {
+      if (inputSum[i] === '$') {
+        inputSum.splice(i, 1);
+        break;
+      }
+    }
+    let myNewSum = inputSum.join('');
+    return myNewSum;
+  }
 }
 
 
--- Donate/utils/utils.js
+++ Donate/utils/utils.js
@@ -1,32 +1,33 @@
-'use strict';
-
-export function getNowDate() {
-  const fullMonth = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
-
-  const date = new Date();
-  const month = fullMonth[date.getMonth()];
-  const day = date.getDay() + 'th';
-  const year = date.getFullYear();
-  const hours = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
-
-  const now = month + ' ' + day + ' ' + year + ', ' + getTwoDigitNumbers(hours) + ':' + getTwoDigitNumbers(min) + ':' + getTwoDigitNumbers(sec) + ' ' + getAmPm(hours);
-  return now;
-}
-
-function getTwoDigitNumbers(number) {
-  if (number < 10) {
-    number = '0' + number;
+export class FormatDate {
+  constructor() {
+    this.fullMonth = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+    this.date = new Date();
+    this.month = this.fullMonth[this.date.getMonth()];
+    this.day = this.date.getDate() + 'th';
+    this.year = this.date.getFullYear();
+    this.hours = this.date.getHours();
+    this.min = this.date.getMinutes();
+    this.sec = this.date.getSeconds();
   }
-  return number;
-}
 
-function getAmPm(hours) {
-  if (hours > 12) {
-    return 'pm';
-  } else {
-    return 'am';
+  getNowDate() {
+    const now = this.month + ' ' + this.day + ' ' + this.year + ', ' + this.#getTwoDigitNumbers(this.hours) + ':' + this.#getTwoDigitNumbers(this.min) + ':' + this.#getTwoDigitNumbers(this.sec) + ' ' + this.#getAmPm(this.hours);
+    return now;
+  }
+
+  #getTwoDigitNumbers(number) {
+    if (number < 10) {
+      number = '0' + number;
+    }
+    return number;
+  }
+
+  #getAmPm(hours) {
+    if (hours > 12) {
+      return 'pm';
+    } else {
+      return 'am';
+    }
   }
 }
 
