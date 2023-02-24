e0d32c6a39e72f3ccd458e08aedae3d84cfdcf35
anastasiia_kazantseva <asyakz@gmail.com>
2023-02-24 23:18:14 +0500

    add Fast-Company-Filter

---
+++ Fast-Company-Filter/src/app/api/index.js
@@ -0,0 +1,10 @@
+import users from "./fake.api/user.api";
+import professions from "./fake.api/professions.api";
+import professionsObject from "./fake.api/professions.api";
+
+const API = {
+  users,
+  professions,
+  professionsObject
+};
+export default API;
